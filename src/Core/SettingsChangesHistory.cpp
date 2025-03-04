e77d16474f4f53f061d1c390e8eba4cf7758890c
liuneng1994 <1398775315@qq.com>
2025-03-04 11:22:47 +0800

    change config name

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -162,7 +162,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"restore_replicated_merge_tree_to_shared_merge_tree", false, false, "New setting."},
             {"parallel_replicas_only_with_analyzer", true, true, "Parallel replicas is supported only with analyzer enabled"},
             {"s3_allow_multipart_copy", true, true, "New setting."},
-            {"input_format_parquet_use_native_reader_with_filter_push_down", false, false, "A new parquet reader support full filter push down"},
+            {"input_format_parquet_use_native_reader_v2", false, false, "A new parquet reader support full filter push down"},
         });
         addSettingsChanges(settings_changes_history, "25.1",
         {
